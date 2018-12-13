a8164b9da5cd0cefa7082f650b153b6caca2cab4
Gaterny <scusong@foxmail.com>
2018-12-13 22:30:10 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from os import path
 
-with open("README.md", "r", encoding='utf8') as f:
+dir_path = path.abspath(path.dirname(__file__))
+print(dir_path)
+with open(path.join(dir_path, "README.md"), encoding='utf-8') as f:
     long_description = f.read()
+
 setup(
     name='sougou-cli',
     version='1.0.0',
@@ -18,18 +22,15 @@ setup(
     license='MIT',
     packages=find_packages(),
     platforms=["all"],
-    install_requires=[
-            'requests',
-        ],
+    install_requires=['requests'],
     url='https://github.com/Gaterny/sougou-cli',
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
     entry_points={
         'console_scripts': [
-            'sg = sougou.sougou:main',
+            'sg=sougou.sougou:main',
         ]
     }
 )
